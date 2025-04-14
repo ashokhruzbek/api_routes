40927bc74239162f304f9e7dfadb611346290cac
shokhruzbek <ashokhruzbek@gmail.com>
2025-04-14 23:22:16 +0500

    some changes and updates

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import Avatar from "../assets/avatar.jpg"
 function CommentCard() {
   return (
-    <div>CommentCard</div>
+    <div class="comment-card">
+      <div class="user-info">
+        <img src={Avatar} alt="User Avatar" class="avatar" />
+        <div>
+          <h3 class="comment-name">John Doe</h3>
+          <span class="comment-email">johndoe@example.com</span>
+        </div>
+      </div>
+      <p class="comment-body">
+        This is a beautiful comment card with gradient background and smooth animations. It includes a profile photo, name, email, and comment text.
+      </p>
+    </div>
+
   )
 }
 
--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import "./PostCard.css"
+import { useNavigate } from 'react-router-dom';
 
-function PostCard({title, post}) {
+function PostCard({ title, post }) {
+
+  const navigate = useNavigate();
+  const handleLogin = (e) => {
+    e.preventDefault();
+    navigate('/comments', { replace: true });
+
+  }
   return (
-    <div className="post-container">
-      <div className="post">
-        <h4 className="post-title">{title}</h4>
-        <p className="post-body">{post}</p>
-      </div>
+    <div onClick={() => navigate('/comments')} className="post-container" style={{ cursor: 'pointer' }}>
+    <div className="post">
+      <h4 className="post-title">{title}</h4>
+      <p className="post-body">{post}</p>
     </div>
+  </div>
   )
 }
 
